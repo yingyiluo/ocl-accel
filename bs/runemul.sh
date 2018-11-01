1f7de0c426e15cfff6f7303c34ed0b0dec8a12f3
Yingyi Luo <yingyiluo@jlselogin1.ftm.alcf.anl.gov>
2018-11-01 20:40:45 +0000

    Update Emulation script for quartus 17.1

--- bs/runemul.sh
+++ bs/runemul.sh
@@ -1,4 +1,4 @@
-aoc -march=emulator device/bs.cl -o bin/bs.aocx
+aoc -march=emulator -emulator-channel-depth-model=strict device/bs_v17.cl -o bin/bs.aocx
 make clean
 make
 CL_CONTEXT_EMULATOR_DEVICE_INTELFPGA=1 ./bin/host
